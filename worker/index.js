d790b391ad5a071dba2453eaae78e491a40e011a
pranay <drpranayaryal@gmail.com>
2019-11-29 23:35:30 -0500

    remove unnecess lines

--- worker/index.js
+++ worker/index.js
@@ -1,14 +1,5 @@
 var CronJob = require('cron').CronJob;
 
-
-var redis = require("redis"),
-    client = redis.createClient();
-
-const {promisify} = require('util');
-const getAsync = promisify(client.get).bind(client);
-
-const setAsync = promisify(client.get).bind(client);
-
 const fetchGithub = require('./tasks/fetch-github');
 
 new CronJob('* * * * * ', fetchGithub, null, true, 'America/Los_Angeles');
