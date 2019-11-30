c293dae32e08dfe926d4f7cef64c97f67d432c7c
pranay <drpranayaryal@gmail.com>
2019-11-30 17:52:09 -0500

    finishing up

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -1,20 +1,39 @@
-import React from 'react'
+/*jshint esversion: 6 */
+import React, { useState } from 'react'
 
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 
+import JobModal from './JobModal';
+
+
+
 const Job = ({ job }) => {
+
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpenModal(true);
+  };
+
+  const handleClose = () => {
+    setOpenModal(false);
+  };
+
   return (
-    <Paper className="job">
-      <div>
-        <Typography variant="h5">{job.title}</Typography>
-        <Typography variant="h6">{job.company.trim()}</Typography>
-        <Typography variant="h7">{job.location.trim()}</Typography>
-      </div>
-      <div>
-        <Typography>{job.created_at.split(' ').slice(0, 3).join(' ')}</Typography>
-      </div>
-    </Paper>
+    <div>
+      <Paper className="job" onClick={handleClickOpen}>
+        <div>
+          <Typography variant="h4">{job.title}</Typography>
+          <Typography variant="h5">{job.company.trim()}</Typography>
+          <Typography variant="h6">{job.location.trim()}</Typography>
+        </div>
+        <div>
+          <Typography>{job.created_at.split(' ').slice(0, 3).join(' ')}</Typography>
+        </div>
+      </Paper>
+      <JobModal job={job} openModal={openModal} handleClose={handleClose} />
+    </div>
   )
 };
 
--- client/src/components/Jobs.js
+++ client/src/components/Jobs.js
@@ -1,17 +1,76 @@
-import React from 'react';
+/*jshint esversion: 6 */
+import React, { useState } from 'react';
 
 import Typography from '@material-ui/core/Typography';
 import Job from './Job';
+import JobModal from './JobModal';
+
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 
 const Jobs = ({ jobs }) => {
 
-    console.log('job is', jobs[0])
+    const numJobs = jobs.length;
+    const numPages = Math.ceil(numJobs / 50);
+
+    const [activeStep, setActiveStep] = useState(0);
+    const [openModal, setOpenModal] = useState(false);
+    
+    const jobsOnPage = jobs.slice(activeStep * 50, (activeStep * 50) + 50);
+
+    const handleNext = () => {
+      setActiveStep(prevActiveStep => prevActiveStep + 1 );
+    }
+
+    const handleBack = () => {
+      setActiveStep(prevActiveStep => prevActiveStep - 1 );
+    }
+
+
+    const handleClickOpen = () => {
+      console.log('youarehere');
+      setOpenModal(true);
+    };
+
+    const handleClose = () => {
+      setOpenModal(false);
+    };
+
     return (
     <div className="jobs">
       <Typography variant="h4" component="h1">
         Entry Level Software Jobs
       </Typography>
-      { jobs.map(job => <Job job={job}/>) }
+      <Typography variant="h6" component="h1">
+        Found {numJobs} jobs.
+      </Typography>
+      { jobsOnPage.map((job, i) => 
+        <Job job={job} key={i}/>)}
+
+      <div>
+        Page {activeStep+ 1 } of {numPages}
+      </div>
+
+      <MobileStepper
+      variant="progress"
+      steps={numPages}
+      position="static"
+      activeStep={activeStep}
+      nextButton={
+        <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+          Next
+          <KeyboardArrowRight />
+        </Button>
+      }
+      backButton={
+        <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+          <KeyboardArrowLeft />
+          Back
+        </Button>
+      }
+    />
 
 
     </div>
